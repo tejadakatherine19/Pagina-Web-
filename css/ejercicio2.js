a0d4e1bd148d6af77a0eaf5529a5b5da55e650be
tejadakatherine19 <67174006+tejadakatherine19@users.noreply.github.com>
2020-12-08 21:21:26 -0600

    subir proyecto

---
+++ css/ejercicio2.js
@@ -0,0 +1,40 @@
+<html>
+
+       <body>
+
+ 
+
+             <script  language – “javascript”  type- “tcxt/javascript”>
+
+        var    pulg = 60 ;
+
+         var   c_pies = ( pulg*0.0833333 ) ;
+
+         var    pies= 3 ;
+
+          var   c_metros= ( pies *0.3048) ;
+
+          var   km= 10 ;
+
+           var   c_millas=( km*0.621371) ;
+
+ 
+
+                             document.write(“Conversión de Unidades : “ ) ;
+
+                             document.write(“ < br > < br > “ ) ;
+
+                             document.write(pulg+ “ pulgadas= “+c_pies+ “ pies.” ) ;
+
+                             document.write( “<br> “) ;
+
+                             document.write(pies+” pies=  “+c_metros+ “ metros . “ ) ;
+
+                             document.write( “< br > “ ) ;
+
+                             document.write(km+” kilómetros= “+c_millas+” millas . “ ) ;
+
+             </ script>
+
+     </html >
+
